39ccbf8d0a98b1765ffc6f8702ff7b0675be7c21
Victor Stelzer Machado <victor@originaal.com.br>
2025-05-14 20:06:22 -0300

    ajuste grafo

--- backend/app/graph.py
+++ backend/app/graph.py
@@ -1,126 +1,126 @@
 campus_graph = {
-    "Entrada Frontal": {
-        "Hall 1": {"distancia": 5, "direcao": "frente"}
+    "EntradaFrontal": {
+        "Hall1": {"distancia": 5, "direcao": "frente"}
     },
-    "Entrada Traseira": {
-        "Hall 1": {"distancia": 7, "direcao": "frente"}
+    "EntradaTraseira": {
+        "Hall1": {"distancia": 7, "direcao": "frente"}
     },
 
-    "Hall 1": {
-        "Entrada Frontal": {"distancia": 5, "direcao": "atrás"},
-        "Entrada Traseira": {"distancia": 7, "direcao": "atrás"},
-        "Corredor 1A": {"distancia": 4, "direcao": "esquerda"},
-        "Corredor 1B": {"distancia": 4, "direcao": "direita"},
-        "Escada 1": {"distancia": 3, "direcao": "frente"},
-        "Escada 2": {"distancia": 3, "direcao": "frente"},
+    "Hall1": {
+        "EntradaFrontal": {"distancia": 5, "direcao": "atrás"},
+        "EntradaTraseira": {"distancia": 7, "direcao": "atrás"},
+        "Corredor1A": {"distancia": 4, "direcao": "esquerda"},
+        "Corredor1B": {"distancia": 4, "direcao": "direita"},
+        "Escada1": {"distancia": 3, "direcao": "frente"},
+        "Escada2": {"distancia": 3, "direcao": "frente"},
         "Rampa": {"distancia": 6, "direcao": "frente"}
     },
 
-    "Corredor 1A": {
-        "Hall 1": {"distancia": 4, "direcao": "direita"},
-        "Sala 102": {"distancia": 3, "direcao": "esquerda"},
-        "Sala 104": {"distancia": 3, "direcao": "frente"},
-        "Sala 106": {"distancia": 3, "direcao": "frente"},
-        "Banheiro 1A": {"distancia": 2, "direcao": "fim do corredor"}
+    "Corredor1A": {
+        "Hall1": {"distancia": 4, "direcao": "direita"},
+        "Sala102": {"distancia": 3, "direcao": "esquerda"},
+        "Sala104": {"distancia": 3, "direcao": "frente"},
+        "Sala106": {"distancia": 3, "direcao": "frente"},
+        "Banheiro1A": {"distancia": 2, "direcao": "fim do corredor"}
     },
-    "Corredor 1B": {
-        "Hall 1": {"distancia": 4, "direcao": "esquerda"},
-        "Sala 101": {"distancia": 3, "direcao": "direita"},
-        "Sala 103": {"distancia": 3, "direcao": "frente"},
-        "Sala 105": {"distancia": 3, "direcao": "frente"},
-        "Banheiro 1B": {"distancia": 2, "direcao": "fim do corredor"}
+    "Corredor1B": {
+        "Hall1": {"distancia": 4, "direcao": "esquerda"},
+        "Sala101": {"distancia": 3, "direcao": "direita"},
+        "Sala103": {"distancia": 3, "direcao": "frente"},
+        "Sala105": {"distancia": 3, "direcao": "frente"},
+        "Banheiro1B": {"distancia": 2, "direcao": "fim do corredor"}
     },
 
-    "Sala 101": {"Corredor 1B": {"distancia": 3, "direcao": "esquerda"}},
-    "Sala 102": {"Corredor 1A": {"distancia": 3, "direcao": "direita"}},
-    "Sala 103": {"Corredor 1B": {"distancia": 3, "direcao": "atrás"}},
-    "Sala 104": {"Corredor 1A": {"distancia": 3, "direcao": "atrás"}},
-    "Sala 105": {"Corredor 1B": {"distancia": 3, "direcao": "atrás"}},
-    "Sala 106": {"Corredor 1A": {"distancia": 3, "direcao": "atrás"}},
-    "Banheiro 1A": {"Corredor 1A": {"distancia": 2, "direcao": "volte"}},
-    "Banheiro 1B": {"Corredor 1B": {"distancia": 2, "direcao": "volte"}},
+    "Sala101": {"Corredor1B": {"distancia": 3, "direcao": "esquerda"}},
+    "Sala102": {"Corredor1A": {"distancia": 3, "direcao": "direita"}},
+    "Sala103": {"Corredor1B": {"distancia": 3, "direcao": "atrás"}},
+    "Sala104": {"Corredor1A": {"distancia": 3, "direcao": "atrás"}},
+    "Sala105": {"Corredor1B": {"distancia": 3, "direcao": "atrás"}},
+    "Sala106": {"Corredor1A": {"distancia": 3, "direcao": "atrás"}},
+    "Banheiro1A": {"Corredor1A": {"distancia": 2, "direcao": "volte"}},
+    "Banheiro1B": {"Corredor1B": {"distancia": 2, "direcao": "volte"}},
 
-    "Escada 1": {
-        "Hall 1": {"distancia": 3, "direcao": "atrás"},
-        "Hall 2": {"distancia": 3, "direcao": "sobe"},
-        "Hall 3": {"distancia": 3, "direcao": "sobe"}
+    "Escada1": {
+        "Hall1": {"distancia": 3, "direcao": "atrás"},
+        "Hall2": {"distancia": 3, "direcao": "sobe"},
+        "Hall3": {"distancia": 3, "direcao": "sobe"}
     },
-    "Escada 2": {
-        "Hall 1": {"distancia": 3, "direcao": "atrás"},
-        "Hall 2": {"distancia": 3, "direcao": "sobe"},
-        "Hall 3": {"distancia": 3, "direcao": "sobe"}
+    "Escada2": {
+        "Hall1": {"distancia": 3, "direcao": "atrás"},
+        "Hall2": {"distancia": 3, "direcao": "sobe"},
+        "Hall3": {"distancia": 3, "direcao": "sobe"}
     },
     "Rampa": {
-        "Hall 1": {"distancia": 6, "direcao": "atrás"},
-        "Hall 2": {"distancia": 6, "direcao": "sobe"},
-        "Hall 3": {"distancia": 6, "direcao": "sobe"}
+        "Hall1": {"distancia": 6, "direcao": "atrás"},
+        "Hall2": {"distancia": 6, "direcao": "sobe"},
+        "Hall3": {"distancia": 6, "direcao": "sobe"}
     },
 
-    "Hall 2": {
-        "Escada 1": {"distancia": 3, "direcao": "desce"},
-        "Escada 2": {"distancia": 3, "direcao": "desce"},
+    "Hall2": {
+        "Escada1": {"distancia": 3, "direcao": "desce"},
+        "Escada2": {"distancia": 3, "direcao": "desce"},
         "Rampa": {"distancia": 6, "direcao": "desce"},
-        "Corredor 2A": {"distancia": 4, "direcao": "esquerda"},
-        "Corredor 2B": {"distancia": 4, "direcao": "direita"},
+        "Corredor2A": {"distancia": 4, "direcao": "esquerda"},
+        "Corredor2B": {"distancia": 4, "direcao": "direita"},
         "Lanchonete": {"distancia": 4, "direcao": "frente"}
     },
 
-    "Corredor 2A": {
-        "Hall 2": {"distancia": 4, "direcao": "direita"},
-        "Sala 202": {"distancia": 3, "direcao": "esquerda"},
-        "Sala 204": {"distancia": 3, "direcao": "frente"},
-        "Sala 206": {"distancia": 3, "direcao": "frente"},
-        "Banheiro 2A": {"distancia": 2, "direcao": "fim do corredor"}
+    "Corredor2A": {
+        "Hall2": {"distancia": 4, "direcao": "direita"},
+        "Sala202": {"distancia": 3, "direcao": "esquerda"},
+        "Sala204": {"distancia": 3, "direcao": "frente"},
+        "Sala206": {"distancia": 3, "direcao": "frente"},
+        "Banheiro2A": {"distancia": 2, "direcao": "fim do corredor"}
     },
-    "Corredor 2B": {
-        "Hall 2": {"distancia": 4, "direcao": "esquerda"},
-        "Sala 201": {"distancia": 3, "direcao": "direita"},
-        "Sala 203": {"distancia": 3, "direcao": "frente"},
-        "Sala 205": {"distancia": 3, "direcao": "frente"},
-        "Banheiro 2B": {"distancia": 2, "direcao": "fim do corredor"}
+    "Corredor2B": {
+        "Hall2": {"distancia": 4, "direcao": "esquerda"},
+        "Sala201": {"distancia": 3, "direcao": "direita"},
+        "Sala203": {"distancia": 3, "direcao": "frente"},
+        "Sala205": {"distancia": 3, "direcao": "frente"},
+        "Banheiro2B": {"distancia": 2, "direcao": "fim do corredor"}
     },
 
-    "Sala 201": {"Corredor 2B": {"distancia": 3, "direcao": "esquerda"}},
-    "Sala 202": {"Corredor 2A": {"distancia": 3, "direcao": "direita"}},
-    "Sala 203": {"Corredor 2B": {"distancia": 3, "direcao": "atrás"}},
-    "Sala 204": {"Corredor 2A": {"distancia": 3, "direcao": "atrás"}},
-    "Sala 205": {"Corredor 2B": {"distancia": 3, "direcao": "atrás"}},
-    "Sala 206": {"Corredor 2A": {"distancia": 3, "direcao": "atrás"}},
-    "Banheiro 2A": {"Corredor 2A": {"distancia": 2, "direcao": "volte"}},
-    "Banheiro 2B": {"Corredor 2B": {"distancia": 2, "direcao": "volte"}},
-    "Lanchonete": {"Hall 2": {"distancia": 4, "direcao": "atrás"}},
+    "Sala201": {"Corredor2B": {"distancia": 3, "direcao": "esquerda"}},
+    "Sala202": {"Corredor2A": {"distancia": 3, "direcao": "direita"}},
+    "Sala203": {"Corredor2B": {"distancia": 3, "direcao": "atrás"}},
+    "Sala204": {"Corredor2A": {"distancia": 3, "direcao": "atrás"}},
+    "Sala205": {"Corredor2B": {"distancia": 3, "direcao": "atrás"}},
+    "Sala206": {"Corredor2A": {"distancia": 3, "direcao": "atrás"}},
+    "Banheiro2A": {"Corredor2A": {"distancia": 2, "direcao": "volte"}},
+    "Banheiro2B": {"Corredor2B": {"distancia": 2, "direcao": "volte"}},
+    "Lanchonete": {"Hall2": {"distancia": 4, "direcao": "atrás"}},
 
-    "Hall 3": {
-        "Escada 1": {"distancia": 3, "direcao": "desce"},
-        "Escada 2": {"distancia": 3, "direcao": "desce"},
+    "Hall3": {
+        "Escada1": {"distancia": 3, "direcao": "desce"},
+        "Escada2": {"distancia": 3, "direcao": "desce"},
         "Rampa": {"distancia": 6, "direcao": "desce"},
-        "Corredor 3A": {"distancia": 4, "direcao": "esquerda"},
-        "Corredor 3B": {"distancia": 4, "direcao": "direita"},
-        "Web Class": {"distancia": 4, "direcao": "frente"}
+        "Corredor3A": {"distancia": 4, "direcao": "esquerda"},
+        "Corredor3B": {"distancia": 4, "direcao": "direita"},
+        "WebClass": {"distancia": 4, "direcao": "frente"}
     },
 
-    "Corredor 3A": {
-        "Hall 3": {"distancia": 4, "direcao": "direita"},
-        "Sala 302": {"distancia": 3, "direcao": "esquerda"},
-        "Sala 304": {"distancia": 3, "direcao": "frente"},
-        "Sala 306": {"distancia": 3, "direcao": "frente"},
-        "Banheiro 3A": {"distancia": 2, "direcao": "fim do corredor"}
+    "Corredor3A": {
+        "Hall3": {"distancia": 4, "direcao": "direita"},
+        "Sala302": {"distancia": 3, "direcao": "esquerda"},
+        "Sala304": {"distancia": 3, "direcao": "frente"},
+        "Sala306": {"distancia": 3, "direcao": "frente"},
+        "Banheiro3A": {"distancia": 2, "direcao": "fim do corredor"}
     },
-    "Corredor 3B": {
-        "Hall 3": {"distancia": 4, "direcao": "esquerda"},
-        "Sala 301": {"distancia": 3, "direcao": "direita"},
-        "Sala 303": {"distancia": 3, "direcao": "frente"},
-        "Sala 305": {"distancia": 3, "direcao": "frente"},
-        "Banheiro 3B": {"distancia": 2, "direcao": "fim do corredor"}
+    "Corredor3B": {
+        "Hall3": {"distancia": 4, "direcao": "esquerda"},
+        "Sala301": {"distancia": 3, "direcao": "direita"},
+        "Sala303": {"distancia": 3, "direcao": "frente"},
+        "Sala305": {"distancia": 3, "direcao": "frente"},
+        "Banheiro3B": {"distancia": 2, "direcao": "fim do corredor"}
     },
 
-    "Sala 301": {"Corredor 3B": {"distancia": 3, "direcao": "esquerda"}},
-    "Sala 302": {"Corredor 3A": {"distancia": 3, "direcao": "direita"}},
-    "Sala 303": {"Corredor 3B": {"distancia": 3, "direcao": "atrás"}},
-    "Sala 304": {"Corredor 3A": {"distancia": 3, "direcao": "atrás"}},
-    "Sala 305": {"Corredor 3B": {"distancia": 3, "direcao": "atrás"}},
-    "Sala 306": {"Corredor 3A": {"distancia": 3, "direcao": "atrás"}},
-    "Banheiro 3A": {"Corredor 3A": {"distancia": 2, "direcao": "volte"}},
-    "Banheiro 3B": {"Corredor 3B": {"distancia": 2, "direcao": "volte"}},
-    "Web Class": {"Hall 3": {"distancia": 4, "direcao": "atrás"}}
+    "Sala301": {"Corredor3B": {"distancia": 3, "direcao": "esquerda"}},
+    "Sala302": {"Corredor3A": {"distancia": 3, "direcao": "direita"}},
+    "Sala303": {"Corredor3B": {"distancia": 3, "direcao": "atrás"}},
+    "Sala304": {"Corredor3A": {"distancia": 3, "direcao": "atrás"}},
+    "Sala305": {"Corredor3B": {"distancia": 3, "direcao": "atrás"}},
+    "Sala306": {"Corredor3A": {"distancia": 3, "direcao": "atrás"}},
+    "Banheiro3A": {"Corredor3A": {"distancia": 2, "direcao": "volte"}},
+    "Banheiro3B": {"Corredor3B": {"distancia": 2, "direcao": "volte"}},
+    "WebClass": {"Hall 3": {"distancia": 4, "direcao": "atrás"}}
 }
